fa54fad1b1ab42fd3f216b3eea93bcb4c0504aa7
Super Jump <superjump.void@gmail.com>
2026-01-09 10:24:19 +0800

    12预设

--- src/presets/arcadion/m12s.ts
+++ src/presets/arcadion/m12s.ts
@@ -114,4 +114,99 @@ const M12S_3L: ArenaPreset = {
     isSpoilerFree: true,
 };
 
-export const ARENA_PRESETS_ARCADION_M12S = [M12S_1, M12S_1L, M12S_2, M12S_2L, M12S_3, M12S_3L];
+const M12S_4: ArenaPreset = {
+    key: 'M12S_4',
+    name: '1m=10px',
+    floor: {
+        shape: FloorShape.Circle,
+        width: 480,
+        height: 480,
+        color: '#000000',
+        opacity: 100,
+    },
+    grid: {
+        type: GridType.None,
+    },
+    texture: {
+        url: 'https://cos.xivstrat.cn/public/floor/07/m12s/4.webp',
+        width: 480,
+        height: 480,
+    },
+    isSpoilerFree: true,
+};
+
+const M12S_4L: ArenaPreset = {
+    key: 'M12S_4L',
+    name: '1m=15px',
+    floor: {
+        shape: FloorShape.Circle,
+        width: 720,
+        height: 720,
+        color: '#000000',
+        opacity: 100,
+    },
+    grid: {
+        type: GridType.None,
+    },
+    texture: {
+        url: 'https://cos.xivstrat.cn/public/floor/07/m12s/4.webp',
+        width: 720,
+        height: 720,
+    },
+    isSpoilerFree: true,
+};
+
+const M12S_5: ArenaPreset = {
+    key: 'M12S_5',
+    name: '1m=10px',
+    floor: {
+        shape: FloorShape.Circle,
+        width: 480,
+        height: 480,
+        color: '#000000',
+        opacity: 100,
+    },
+    grid: {
+        type: GridType.None,
+    },
+    texture: {
+        url: 'https://cos.xivstrat.cn/public/floor/07/m12s/5.webp',
+        width: 480,
+        height: 480,
+    },
+    isSpoilerFree: true,
+};
+
+const M12S_5L: ArenaPreset = {
+    key: 'M12S_5L',
+    name: '1m=15px',
+    floor: {
+        shape: FloorShape.Circle,
+        width: 720,
+        height: 720,
+        color: '#000000',
+        opacity: 100,
+    },
+    grid: {
+        type: GridType.None,
+    },
+    texture: {
+        url: 'https://cos.xivstrat.cn/public/floor/07/m12s/5.webp',
+        width: 720,
+        height: 720,
+    },
+    isSpoilerFree: true,
+};
+
+export const ARENA_PRESETS_ARCADION_M12S = [
+    M12S_1,
+    M12S_1L,
+    M12S_2,
+    M12S_2L,
+    M12S_3,
+    M12S_3L,
+    M12S_4,
+    M12S_4L,
+    M12S_5,
+    M12S_5L,
+];
