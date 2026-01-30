ff728927618fa4628f02a5d669f21ddfa982e005
Super Jump <superjump.void@gmail.com>
2026-01-30 13:41:01 +0800

    account

--- src/auth/client.ts
+++ src/auth/client.ts
@@ -1,11 +1,9 @@
 import { localization } from 'better-auth-localization';
-import { usernameClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
     baseURL: import.meta.env.VITE_SERVER_URL,
     plugins: [
-        usernameClient(),
         localization({
             defaultLocale: 'zh-Hans',
             fallbackLocale: 'default',
